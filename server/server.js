98932c761822d1f45fc16fb900abd9f2842cdd0a
Ujjwal Singh <vilenofficial664@gmail.com>
2026-08-31 17:41:20 +0530

    Update CORS origin for voting application

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.listen(env.PORT, () => {
 
 app.use(
   cors({
-    origin: "https://vote-secure-a-voting-application-sn.vercel.app",
+    origin: "https://vote-secure-a-voting-application1.vercel.app",
     credentials: true,
   })
 );
