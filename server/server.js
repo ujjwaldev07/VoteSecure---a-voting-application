f0b705ca6cb62ce7257bd34c32ce5da3ff5cc38e
sweetsoul5 <vilenofficial664@gmail.com>
2026-08-31 16:39:58 +0530

    Fix production CORS origin

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.listen(env.PORT, () => {
 
 app.use(
   cors({
-    origin: "https://vote-secure-a-voting-application-no.vercel.app/",
+    origin: "https://vote-secure-a-voting-application-cf.vercel.app",
     credentials: true,
   })
 );
